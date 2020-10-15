b83f39623be18748578910aaa385c37f6ae5af55
martadossantos <hello@martasantos.co.uk>
2020-10-15 18:55:27 +0100

    added files

---
+++ scripts.js
@@ -0,0 +1,47 @@
+barba.init({
+   debug: true,
+   transitions: [
+      {
+         name: "next",
+         leave({ current, next, trigger }) {
+            return new Promise(resolve => {
+               const timeline = gsap.timeline({
+                  
+                  onComplete() {
+                     current.container.remove()
+                     resolve()
+                  }
+               })
+
+               const navigation = current.container.querySelectorAll("header, a.next, a.previous")
+               const photos = current.container.querySelectorAll("div.photos")
+
+               timeline 
+                  .to(navigation, { opacity: 0 }, 0)
+                  .to(photos, { opacity: 0, x: 500 })
+               
+            })
+         },
+         enter({ current, next, trigger }) {
+            return new Promise(resolve => {
+               const timeline = gsap.timeline({
+                  onComplete() {
+                     resolve()
+                  }
+               })
+
+               const navigation = next.container.querySelectorAll("header, a.next, a.previous")
+               const photos = next.container.querySelectorAll("div.photos")
+
+               timeline
+                  .set(navigation, { opacity: 0 })
+                  .set(photos, { opacity: 0, x: -500 })
+                  .to(navigation, { opacity: 1 }, 0)
+                  .to(photos, { opacity: 0, x: 0 }, 0)
+
+            })
+         }
+      }
+   ],
+   views: []
+})
